d20b62160bd5e6e1e9454470ca9f7575e22e366d
clark <clark@DESKTOP-T9UBNN5>
2022-01-03 12:16:57 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Main(string[] args)
+{
+    Console.WriteLine("1. List Assignments \n2. Add A New Assignment \n3. List Due Dates\n4. Quit");
+    Console.WriteLine("Please Select One Option");
+    bool exit = false;
+
+    while (!exit)
+    {
+        String userCollection = Console.ReadLine();
+
+
+        switch (userCollection)
+        {
+            case "1":
+
+                break;
+            case "2":
+                break;
+
+            case "3":
+                break;
+
+            case "4":
+                break;
+
+            default:
+                continue;
+
+
+
+        }
+    }
+}
+
+
+ 
+
+Main(args);
