8d57449727702652c340455bc37fd5bb53fc5b23
Ryan Clark <42229549+ryanclark532@users.noreply.github.com>
2022-01-07 18:44:58 +1000

    Setup basic command line flow
    Built out non-persistent functions. Starting to build persistent functions with local MSSQL database

---
+++ Assignment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+using System.Data.SqlClient;
+public class Assignment
+{
+	public String title;
+	String description;
+	String subject;
+	public DateOnly dueDate;
+	string connection;
+
+	public Assignment(String title, String description, String subject, DateOnly dueDate, string connection)
+	{
+		this.title = title;
+		this.description = description;
+		this.subject = subject;
+		this.dueDate = dueDate;
+		this.connection = connection;
+
+
+	}
+    public override string ToString()
+    {
+		String s = String.Format("######################\n{0}\n{1}\n{2}	{3}", this.title,this.description,this.subject,this.dueDate);
+		return s;
+    }
+	public void save()
+    {
+		string query = "INSERT INTO Assignment VALUES ('@description', '@subject', '@dueDate', '@title')";
+		using (SqlConnection conn = new SqlConnection(connection))
+            {
+				SqlCommand cmd = new SqlCommand(query, conn);
+				cmd.Parameters.AddWithValue("@title", this.title);
+				cmd.Parameters.AddWithValue("@description", this.description);
+				cmd.Parameters.AddWithValue("@subject", this.subject);
+				cmd.Parameters.AddWithValue("@dueDate", this.dueDate.ToString());
+				conn.Open();
+				int result = cmd.ExecuteNonQuery();
+			    conn.Close();
+			}
+	}
+	static void getAllAssignments()
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,38 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
+using System.Data.SqlClient;
 static void Main(string[] args)
 {
-    Console.WriteLine("1. List Assignments \n2. Add A New Assignment \n3. List Due Dates\n4. Quit");
-    Console.WriteLine("Please Select One Option");
+    String conString = "Data Source=localhost;Initial Catalog=AssignmentDB;Persist Security Info=True;User ID=SA;Password=Ryan1234";
+   
     bool exit = false;
-
+    List<Assignment> assignments = new List<Assignment>();
     while (!exit)
     {
+
+        Console.WriteLine("1. List Assignments \n2. Add A New Assignment \n3. List Due Dates\n4. Quit\nPlease Select One Option");
         String userCollection = Console.ReadLine();
-
-
         switch (userCollection)
         {
             case "1":
-
+                if(assignments.Count<=0)
+                {
+                    Console.WriteLine("You Currently Dont Have Any Assignemnts");
+                }
+                else
+                {
+                    foreach (Assignment assignment in assignments)
+                    {
+                        Console.WriteLine(assignment.ToString());
+                    }
+                }
+                
                 break;
             case "2":
+
+                Console.WriteLine("Title: ");
+                String title = Console.ReadLine();
+
+                Console.WriteLine("Description");
+                String description = Console.ReadLine();
+
+                Console.WriteLine("Subject: ");
+                String subject = Console.ReadLine();
+
+                Console.WriteLine("Due Date, in dd/MM/YY format: ");
+                String date = Console.ReadLine();
+
+                DateOnly dueDate =DateOnly.Parse(date);
+                Console.WriteLine(String.Format("{0}/{1}/{2}", dueDate.Day, dueDate.Month, dueDate.Year));
+                Assignment a = new Assignment(title, description, subject, dueDate, conString);
+                a.save();
+                assignments.Add(a);
                 break;
 
             case "3":
+                foreach (Assignment assignment in assignments.OrderBy(x => x.dueDate).ToList())
+                {
+                    Console.WriteLine(String.Format("{0}    {1}", assignment.dueDate, assignment.title));
+                }
+
+
                 break;
 
             case "4":
+                exit = true;
                 break;
 
             default:
-                continue;
-
-
-
+               continue;
         }
     }
+    
+    Console.WriteLine("Thank You For Using!");
 }
+
+
 
 
  
